8a7f34e88f53ad4e2c853f187647dd0d1beb531a
hallowsman <605137536@qq.com>
2023-01-13 22:57:56 +0800

    initial

---
+++ booktest/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+# 图书类
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField()
+
+    def __str__(self):
+        return "%d" % self.pk
+
+
+# 英雄类
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=200)
+    hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%d' % self.pk
