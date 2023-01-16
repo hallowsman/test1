aa69969faeeb59e0dfe9f1802a6cf7efca52b01f
五七之间 <605137536@qq.com>
2023-01-16 16:42:09 +0800

    url地址配置 python3 目前不对

--- test1/urls.py
+++ test1/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.template.defaulttags import url
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('booktest.urls'))
+    url(r'^', include('booktest.urls')),
 ]
