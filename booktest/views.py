7ae2fd3e9d45ae35bd6cd3bd7508a49a92e46cad
五七之间 <605137536@qq.com>
2023-01-17 16:13:53 +0800

    no message

--- booktest/views.py
+++ booktest/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import *
+from django.template import RequestContext, loader
+from .models import *
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('index')
+    # temp = loader.get_template('booktest/index.html')
+    # return HttpResponse(temp.render())
+    context = {'title': '123'}
+
+    booklist = BookInfo.objects.all()
+    context = {'list': booklist}
+
+    return render(request, 'booktest/index.html', context)
--- test1/urls.py
+++ test1/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('booktest.urls')),
+    path(r'booktest/', include('booktest.urls')),
+    # path('^', include('booktest.urls')),
 ]
